5d6298acda471120cc2682de73cf4c691c1aaad9
Mattheus Macedo <mattheus.macedo@novatendencia.localdomain>
2020-09-02 14:53:51 -0300

    Primeiro commit aulaJPA

---
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,30 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Zecass da Souza", "souza@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Welligton de Macedo", "wellington@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Feito!");
+		
+		
+		
+	}
+
+}
